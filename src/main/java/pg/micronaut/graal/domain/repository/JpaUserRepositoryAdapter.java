9c81dace7a6aa83d0567cbebffe432f1478c26ca
Alexey Lapin <alexey-lapin@protonmail.com>
2021-03-02 23:11:14 +0300

    add code

--- src/main/java/pg/micronaut/graal/domain/repository/JpaUserRepositoryAdapter.java
+++ src/main/java/pg/micronaut/graal/domain/repository/JpaUserRepositoryAdapter.java
@@ -1,11 +1,20 @@
 package pg.micronaut.graal.domain.repository;
 
+import io.micronaut.core.annotation.TypeHint;
 import lombok.RequiredArgsConstructor;
 import pg.micronaut.graal.domain.model.User;
 
 import javax.inject.Singleton;
 import java.util.Optional;
 
+@TypeHint(
+        value = {JpaUserRepositoryAdapter.class},
+        accessType = {
+                TypeHint.AccessType.ALL_DECLARED_CONSTRUCTORS,
+                TypeHint.AccessType.ALL_DECLARED_FIELDS,
+                TypeHint.AccessType.ALL_PUBLIC_METHODS
+        }
+)
 @RequiredArgsConstructor
 @Singleton
 public class JpaUserRepositoryAdapter implements UserRepository {
