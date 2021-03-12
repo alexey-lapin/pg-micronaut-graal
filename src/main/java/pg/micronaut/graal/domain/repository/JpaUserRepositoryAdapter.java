de258bee4068510ae25a23c41367c0021e1df4fe
Alexey Lapin <alexey-lapin@protonmail.com>
2021-03-12 22:01:00 +0300

    try some logs

--- src/main/java/pg/micronaut/graal/domain/repository/JpaUserRepositoryAdapter.java
+++ src/main/java/pg/micronaut/graal/domain/repository/JpaUserRepositoryAdapter.java
@@ -1,7 +1,12 @@
 package pg.micronaut.graal.domain.repository;
 
+import io.micronaut.context.ApplicationContext;
 import io.micronaut.core.annotation.Introspected;
 import io.micronaut.core.annotation.TypeHint;
+import io.micronaut.core.beans.BeanWrapper;
+import io.micronaut.core.reflect.ReflectionUtils;
+import io.micronaut.inject.BeanDefinition;
+import io.micronaut.inject.ExecutableMethod;
 import lombok.RequiredArgsConstructor;
 import pg.micronaut.graal.domain.model.User;
 
@@ -23,6 +28,7 @@ import java.util.UUID;
 public class JpaUserRepositoryAdapter implements UserRepository {
 
     private final DataUserRepository repository;
+    private final ApplicationContext context;
 
     @Override
     public Optional<User> findByEmail(String email) {
@@ -37,6 +43,10 @@ public class JpaUserRepositoryAdapter implements UserRepository {
     @Override
     public User save(User user) {
         UUID id = user.getId();
+        System.out.println(">> " + id.getClass().getName());
+        BeanDefinition<? extends DataUserRepository> beanDefinition = context.getBeanDefinition(repository.getClass());
+        Optional<? extends ExecutableMethod<? extends DataUserRepository, Object>> existsById = beanDefinition.findMethod("existsById", UUID.class);
+        System.out.println(">> " + existsById.get());
         boolean b = repository.existsById(id);
         if (b) {
             return repository.update(user);
