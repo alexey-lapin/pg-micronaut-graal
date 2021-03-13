66b4993e42b93e051ec0e8353c24cae5e15b0708
Alexey Lapin <alexey-lapin@protonmail.com>
2021-03-13 16:07:18 +0300

    try more type hints

---
+++ src/main/java/pg/micronaut/graal/config/UUIDConfig.java
@@ -0,0 +1,9 @@
+package pg.micronaut.graal.config;
+
+import io.micronaut.core.annotation.Introspected;
+
+import java.util.UUID;
+
+@Introspected(classes = UUID.class)
+public class UUIDConfig {
+}
